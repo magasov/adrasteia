1aefa5d9b494001406464dbdf56583a0a3d9fbf6
magaasov <magaasov>
2024-05-20 13:27:02 +0300

    magasov

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function popupClose() {
+    let close = document.querySelector("#popup")
+    close.style.display = "none"
+}
+function popupYes() {
+    let close = document.querySelector("#popup")
+    close.style.display = "block"
+}
